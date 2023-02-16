bb70b2d712355f7670f7a9550d5b4e5e96af0aca
ahyiru <ah.yiru@gmail.com>
2023-02-16 11:39:08 +0800

    fix: fix configs

--- jest.config.js
+++ jest.config.js
@@ -15,14 +15,13 @@ module.exports = {
   },
   coveragePathIgnorePatterns: ['/node_modules/'],
   testRegex: '(/__tests__/.*\\.(test|spec))\\.(tsx?|jsx?)$',
-  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/commons/', '<rootDir>/playground/', '<rootDir>/draft/'],
+  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/server/', '<rootDir>/playground/', '<rootDir>/draft/'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'mjs', 'json', 'node', 'vue'],
   modulePathIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     '@app(.*)$': '<rootDir>/app/$1',
-    '@commons(.*)$': ['<rootDir>/commons/layout/$1', '<rootDir>/commons/styles/$1'],
   },
   transform: {
     // '^.+\\.vue$': 'vue-jest',
