24ccf2c0e3e4294bcb88eba4321955d0115ab3ac
ahyiru <ah.yiru@gmail.com>
2023-02-16 17:21:44 +0800

    fix: fix init files

--- configs/index.js
+++ configs/index.js
@@ -12,40 +12,40 @@ const initAppConfig = async () => {
   const exists = await fs.pathExists(userConfigDir);
   if (!exists) {
     await fs.ensureDir(resolve(rootDir, './.huxy'));
-    await fs.copy(huxyConfigDir, userConfigDir);
+    fs.copy(huxyConfigDir, userConfigDir);
   } else {
-    await fs.copy(userConfigDir, huxyConfigDir);
+    fs.copy(userConfigDir, huxyConfigDir);
   }
-  await initConfigFiles();
-  await initHuskyFiles();
+  initConfigFiles();
+  initHuskyFiles();
 };
 
 const initConfigFile = async (userConfigDir, huxyConfigDir) => {
   const exists = await fs.pathExists(userConfigDir);
   if (!exists) {
-    await fs.copy(huxyConfigDir, userConfigDir);
+    fs.copy(huxyConfigDir, userConfigDir);
   }
 };
 
-const initConfigFiles = async () => {
-  await initConfigFile(resolve(rootDir, './.eslintrc.js'), resolve(sharedDir, './.eslintrc.js'));
-  await initConfigFile(resolve(rootDir, './.stylelintrc.js'), resolve(sharedDir, './.stylelintrc.js'));
-  await initConfigFile(resolve(rootDir, './commitlint.config.js'), resolve(sharedDir, './commitlint.config.js'));
-  await initConfigFile(resolve(rootDir, './jest.config.js'), resolve(sharedDir, './jest.config.js'));
-  await initConfigFile(resolve(rootDir, './postcss.config.js'), resolve(sharedDir, './postcss.config.js'));
-  await initConfigFile(resolve(rootDir, './babel.config.js'), resolve(sharedDir, './babel.config.js'));
-  await initConfigFile(resolve(rootDir, './prettier.config.js'), resolve(sharedDir, './prettier.config.js'));
-  await initConfigFile(resolve(rootDir, './.versionrc.js'), resolve(sharedDir, './.versionrc.js'));
+const initConfigFiles = () => {
+  initConfigFile(resolve(rootDir, './.eslintrc.js'), resolve(sharedDir, './.eslintrc.js'));
+  initConfigFile(resolve(rootDir, './.stylelintrc.js'), resolve(sharedDir, './.stylelintrc.js'));
+  initConfigFile(resolve(rootDir, './commitlint.config.js'), resolve(sharedDir, './commitlint.config.js'));
+  initConfigFile(resolve(rootDir, './jest.config.js'), resolve(sharedDir, './jest.config.js'));
+  initConfigFile(resolve(rootDir, './postcss.config.js'), resolve(sharedDir, './postcss.config.js'));
+  initConfigFile(resolve(rootDir, './babel.config.js'), resolve(sharedDir, './babel.config.js'));
+  initConfigFile(resolve(rootDir, './prettier.config.js'), resolve(sharedDir, './prettier.config.js'));
+  initConfigFile(resolve(rootDir, './.versionrc.js'), resolve(sharedDir, './.versionrc.js'));
   
-  await initConfigFile(resolve(rootDir, './.prettierignore'), resolve(packDir, './.prettierignore'));
-  await initConfigFile(resolve(rootDir, './.browserslistrc'), resolve(packDir, './.browserslistrc'));
-  await initConfigFile(resolve(rootDir, './.editorconfig'), resolve(packDir, './.editorconfig'));
+  initConfigFile(resolve(rootDir, './.prettierignore'), resolve(packDir, './.prettierignore'));
+  initConfigFile(resolve(rootDir, './.browserslistrc'), resolve(packDir, './.browserslistrc'));
+  initConfigFile(resolve(rootDir, './.editorconfig'), resolve(packDir, './.editorconfig'));
 };
 
 const initHuskyFiles = async () => {
   await fs.ensureDir(resolve(rootDir, './.husky'));
-  await initConfigFile(resolve(rootDir, './.husky/commit-msg'), resolve(packDir, './.husky/commit-msg'));
-  await initConfigFile(resolve(rootDir, './.husky/pre-commit'), resolve(packDir, './.husky/pre-commit'));
+  initConfigFile(resolve(rootDir, './.husky/commit-msg'), resolve(packDir, './.husky/commit-msg'));
+  initConfigFile(resolve(rootDir, './.husky/pre-commit'), resolve(packDir, './.husky/pre-commit'));
 };
 
 module.exports = initAppConfig;
