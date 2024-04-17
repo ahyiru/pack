4196514a69e83daed689e87f93e065bcec213d74
ahyiru <ah.yiru@gmail.com>
2024-04-17 21:02:46 +0800

    fix: fix eslint

--- configs/shared/eslint.config.js
+++ configs/shared/eslint.config.js
@@ -1,7 +1,7 @@
 // import babelParser from '@babel/eslint-parser';
 // import babelPlugin from '@babel/eslint-plugin';
-// import reactPlugin from 'eslint-plugin-react';
-// import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import js from "@eslint/js";
 import globals from "globals";
 
@@ -57,23 +57,23 @@ const configs = (customCfgs = []) => [
       'no-restricted-globals': [2, 'event'],
     },
   },
-  // {
-  //   files: ['**/*.jsx'],
-  //   plugins: {
-  //     react: reactPlugin,
-  //     'react-hooks': reactHooksPlugin,
-  //   },
-  //   rules: {
-  //     'react/jsx-uses-vars': 2,
-  //     'react/jsx-no-undef': 2,
-  //     'react/jsx-pascal-case': 1,
-  //     'react/require-render-return': 2,
-  //     'react/self-closing-comp': 2,
-  //     
-  //     'react-hooks/rules-of-hooks': 2,
-  //     'react-hooks/exhaustive-deps': 1,
-  //   },
-  // },
+  {
+    files: ['**/*.jsx'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      'react/jsx-pascal-case': 1,
+      'react/self-closing-comp': 2,
+      // 'react/require-render-return': 2,
+      // 'react/jsx-uses-vars': 2,
+      // 'react/jsx-no-undef': 2,
+      
+      'react-hooks/rules-of-hooks': 2,
+      // 'react-hooks/exhaustive-deps': 1,
+    },
+  },
   // {
   //   settings: {
   //     'import/ignore': ['**/node_modules/', 'coverage/', '**/build/', '**/draft/'],
