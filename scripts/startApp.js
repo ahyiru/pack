808f1ca294bee7ed40b134603d6bd5809924d332
ahyiru <ah.yiru@gmail.com>
2026-08-10 14:28:44 +0800

    fix: fix huxy-node-server

--- scripts/startApp.js
+++ scripts/startApp.js
@@ -1,16 +1,16 @@
-import {startApp, startStaticApp} from 'huxy-server';
+import {startServer, startStatic} from 'huxy-node-server';
 
 import getEnvConfigs from './envConfigs.js';
 import startDev from './startDev.js';
 
-const startServer = async isDev => {
+const startApp = async isDev => {
   process.env.NODE_ENV = isDev ? 'development' : 'production';
   const {appName, HOST, buildPath, proxys, devEnv, prodEnv, nodeServer} = await getEnvConfigs();
   const {port, basepath} = isDev ? devEnv : prodEnv;
-  const server = isDev ? startApp : startStaticApp;
+  const server = isDev ? startServer : startStatic;
   return server({
     appName, HOST, buildPath, proxys, port, basepath
   }, isDev ? startDev(nodeServer) : nodeServer);
 };
 
-export default startServer;
+export default startApp;
